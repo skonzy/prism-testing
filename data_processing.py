dde7a00b3ce7d819e128dd47b3e3914a047fe37c
Nate Skonnard <nate@skonnard.com>
2024-11-26 15:23:16 -0700

    more edits

--- data_processing.py
+++ data_processing.py
@@ -34,3 +34,12 @@ def calculate_correlation(x, y):
     numerator = sum((x[i] - mean_x) * (y[i] - mean_y) for i in range(n))
     denominator = math.sqrt(sum((x[i] - mean_x) ** 2 for i in range(n))) * math.sqrt(sum((y[i] - mean_y) ** 2 for i in range(n)))
     return numerator / denominator
+
+def calculate_covariance(x, y):
+    """
+    Calculates the covariance between two lists of numbers.
+    """
+    mean_x = calculate_mean(x)
+    mean_y = calculate_mean(y)
+    n = len(x)
+    return sum((x[i] - mean_x) * (y[i] - mean_y) for i in range(n)) / n
