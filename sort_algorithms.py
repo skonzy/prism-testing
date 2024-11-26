63bc94bb8f70f09cb994dccb12337a2e71961616
Nate Skonnard <nate@skonnard.com>
2024-11-26 16:10:04 -0700

    merge

--- sort_algorithms.py
+++ sort_algorithms.py
@@ -32,3 +32,20 @@ def mergesort(arr):
     left = mergesort(arr[:mid])
     right = mergesort(arr[mid:])
     return merge(left, right)
+
+def merge(left, right):
+    """
+    Merges two sorted lists.
+    """
+    result = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
