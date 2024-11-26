7d566ed0b9e793ca76eed73c619a76484dbac6c2
Nate Skonnard <nate@skonnard.com>
2024-11-26 15:20:30 -0700

    correlate

--- data_processing.py
+++ data_processing.py
@@ -23,3 +23,14 @@ def calculate_standard_deviation(numbers):
     mean = calculate_mean(numbers)
     variance = sum((x - mean) ** 2 for x in numbers) / len(numbers)
     return math.sqrt(variance)
+
+def calculate_correlation(x, y):
+    """
+    Calculates the correlation between two lists of numbers.
+    """
+    mean_x = calculate_mean(x)
+    mean_y = calculate_mean(y)
+    n = len(x)
+    numerator = sum((x[i] - mean_x) * (y[i] - mean_y) for i in range(n))
+    denominator = math.sqrt(sum((x[i] - mean_x) ** 2 for i in range(n))) * math.sqrt(sum((y[i] - mean_y) ** 2 for i in range(n)))
+    return numerator / denominator
