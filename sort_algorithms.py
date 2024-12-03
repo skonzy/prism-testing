6e5f59c27bc064871cd2baf8344258ea7fbdc88e
Nate Skonnard <nate@skonnard.com>
2024-12-03 16:17:35 -0700

    heapify

--- sort_algorithms.py
+++ sort_algorithms.py
@@ -143,3 +143,18 @@ def heapsort(arr):
         arr[i], arr[0] = arr[0], arr[i]
         heapify(arr, i, 0)
     return arr
+
+def heapify(arr, n, i):
+    """
+    Helper function for heapsort.
+    """
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+    if left < n and arr[i] < arr[left]:
+        largest = left
+    if right < n and arr[largest] < arr[right]:
+        largest = right
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
